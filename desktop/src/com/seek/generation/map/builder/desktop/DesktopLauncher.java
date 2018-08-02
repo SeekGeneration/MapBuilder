94bccb4085d53b22580c8f235c448736e738e939
Seek Generation <seekgeneration@gmail.com>
2018-08-02 15:49:39 +0100

    project setup

---
+++ desktop/src/com/seek/generation/map/builder/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.seek.generation.map.builder.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.seek.generation.map.builder.MapBuilder;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MapBuilder(), config);
+	}
+}
